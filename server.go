e34354bb055da12cdb9ac776b5b4f47f5f024918
Jesse Millar <hellojessemillar@gmail.com>
2016-05-04 09:30:34 -0600

    Added files via upload

---
+++ server.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/byuoitav/av-api/controllers"
+	"github.com/byuoitav/av-api/packages/hateoas"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/fasthttp"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	err := hateoas.Load("https://raw.githubusercontent.com/byuoitav/av-api/master/swagger.yaml")
+	if err != nil {
+		fmt.Printf("Could not load swagger.yaml file. Error: %s", err.Error())
+		panic(err)
+	}
+
+	port := ":8000"
+	e := echo.New()
+	e.Pre(middleware.RemoveTrailingSlash())
+
+	// GET requests
+	e.Get("/", controllers.Root)
+
+	e.Get("/health", controllers.Health)
+
+	e.Get("/rooms", controllers.GetAllRooms)
+	e.Get("/rooms/:room", controllers.GetRoomByName)
+
+	e.Get("/buildings", controllers.GetAllBuildings)
+	e.Get("/buildings/:building", controllers.GetBuildingByName)
+	e.Get("/buildings/:building/rooms", controllers.GetAllRoomsByBuilding)
+	e.Get("/buildings/:building/rooms/:room", controllers.GetRoomByNameAndBuilding)
+	e.Get("/buildings/:building/rooms/:room/signals", controllers.GetAllSignalsByRoomAndBuilding)
+	e.Get("/buildings/:building/rooms/:room/signals/:signal", controllers.GetSignalByRoomAndBuilding)
+
+	// POST requests
+	e.Post("/rooms", controllers.UnimplementedResponse)
+	e.Post("/buildings", controllers.UnimplementedResponse)
+	e.Post("/buildings/:building/rooms/:room/signals", controllers.UnimplementedResponse)
+
+	// PUT requests
+	e.Put("/rooms/:room", controllers.UnimplementedResponse)
+	e.Put("/buildings/:building", controllers.UnimplementedResponse)
+	e.Put("/buildings/:building/rooms/:room", controllers.UnimplementedResponse)
+	e.Put("/buildings/:building/rooms/:room/signals/:signal", controllers.UnimplementedResponse)
+
+	// DELETE requests
+	e.Delete("/rooms/:room", controllers.UnimplementedResponse)
+	e.Delete("/buildings/:building", controllers.UnimplementedResponse)
+	e.Delete("/buildings/:building/rooms/:room", controllers.UnimplementedResponse)
+	e.Delete("/buildings/:building/rooms/:room/signals/:signal", controllers.UnimplementedResponse)
+
+	fmt.Printf("AV API is listening on %s\n", port)
+	e.Run(fasthttp.New(port))
+}
